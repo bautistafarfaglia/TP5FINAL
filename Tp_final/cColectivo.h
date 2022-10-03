eee2a68e8da2dda0d1523f70fd985c2b68d06bea
adriel levy <adriellevy2014@hotmail.com>
2022-10-03 18:09:39 -0300

    cColectivo modificacion

--- Tp_final/cColectivo.h
+++ Tp_final/cColectivo.h
@@ -10,7 +10,6 @@
 
 class cColectivo {
 public:
-    unsigned short int cantidad_actual_pasajeros;
 
     cColectivo(string id_colectivo, bool estado_operativo, cFecha fecha_ultimo_mantenimiento, cColectivero colectivero, cSistemaDePagos sistema_de_pagos, cRecorrido recorrido, unsigned int pos_del_recorrido, string GPS, eSentidoRecorrido sentido, const short int cantidad_max_pasajeros);
 
@@ -28,49 +27,31 @@ public:
 
     unsigned int get_posicion_recorrido();
 
-    /**
-     * @param cRecorrido* nuevo_recorrido
-     */
     void set_nuevo_recorrido(cRecorrido* nuevo_recorrido);
+    
+   void set_colectivero(cColectivero* colectivero);
 
-    /**
-     * @param cColectivero* colectivero
-     */
-    void set_colectivero(cColectivero* colectivero);
+   void set_fecha_mantenimiento(cFecha* fecha);
 
-    /**
-     * @param cFecha* fecha
-     */
-    void set_fecha_mantenimiento(cFecha* fecha);
+   bool control_sentido_pasajero(cPasajeros* pasajero);
 
-    /**
-     * @param cPasajero* pasajero
-     */
-    bool control_sentido_pasajero(cPasajeros* pasajero);
+   void avanzar_recorrido();
 
-    void avanzar_recorrido();
+   bool bajar_pasajeros();
 
-    bool bajar_pasajeros();
+   bool subir_pasajeros(cPasajeros* nuevo_pasajero);
 
-    /**
-     * @param cPasajero* nuevo_pasajero
-     */
-    bool subir_pasajeros(cPasajeros* nuevo_pasajero);
+   void cobrar_boleto(cPasajeros* nuevo_pasajero);
 
-    /**
-     * @param cPasajero* nuevo_pasajero
-     */
-    void cobrar_boleto(cPasajeros* nuevo_pasajero);
+   void cambio_de_sentido_recorrido();
 
-    void cambio_de_sentido_recorrido();
+   void actualizar_GPS();
 
-    void actualizar_GPS();
+   virtual void averia() =0;
 
-    virtual void averia() =0;
+   virtual string to_string_colectivo() =0;
 
-    virtual string to_string_colectivo() =0;
-
-    virtual void imprimir()=0;
+   virtual void imprimir()=0;
 
 protected:
     string const id_colectivo;
@@ -85,6 +66,8 @@ protected:
     string GPS;
     eSentidoRecorrido sentido;
     const unsigned short int cantidad_max_pasajeros;
+    unsigned short int cantidad_actual_pasajeros;
+
 };
 
 #endif //_CCOLECTIVO_H
