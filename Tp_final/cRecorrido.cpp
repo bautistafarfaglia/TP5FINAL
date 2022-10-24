a95b08668d66156166bf37a1e0ed444a6a5dc31c
adriel levy <adriellevy2014@hotmail.com>
2022-10-24 14:22:57 -0300

    Generacion de colecitvo, sin errores dentro del metodo

--- Tp_final/cRecorrido.cpp
+++ Tp_final/cRecorrido.cpp
@@ -43,15 +43,14 @@ istream& operator>>(istream& is, cRecorrido& re)
     cout << "Ingrese los datos del recorrido:" << endl;
     cout << "cantidad de paradas a agregar:" << endl;
     is >> pos; 
-
     cParada* paradaaux;
-    re.get_lista_paradas().resize(pos);//chequear el rezise
+    re.listaParada.resize(pos);
     for (int i = 0; i < pos; i++) {
         int cant = re.get_cantidad_paradas() + i;
         paradaaux = new cParada();
         is >> *paradaaux;
-        re.get_lista_paradas().push_back(paradaaux); //revisar este codigo, algo estaba mal no me acuerdo
-        cout << re.get_lista_paradas().data(); //chequear como que no me toma la direccion de memoria del vector lista y no puedo acceder a ningun dato del mismo, 
+        re.listaParada[i] = paradaaux; 
     }
+    int cal = 0;
     return is;
 }
